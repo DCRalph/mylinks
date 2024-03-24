d45893d5eda4da8a1358d4173937c02bf60c6add
Technoblade <william@williamgiles.co.nz>
2024-03-24 18:08:43 +1300

    change build script

--- buildandstart.sh
+++ buildandstart.sh
@@ -1,6 +1,18 @@
+echo "Starting build and start process"
+echo "Pulling latest changes from git"
 git pull
+
+echo "Removing .next folder"
 rm -rf .next
+
+echo "Installing dependencies"
 npm i
+
+echo "Running postinstall"
 npm run postinstall
+
+echo "Running build"
 npm run build
+
+echo "Starting the server"
 npm run start
