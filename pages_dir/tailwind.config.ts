6b8b5737e98e515a2c6ad274a8478d7d42fd30d9
Technoblade <william@williamgiles.co.nz>
2024-10-21 12:50:27 +1300

    appe dir

--- pages_dir/tailwind.config.ts
+++
@@ -1,9 +0,0 @@
-import { type Config } from "tailwindcss";
-
-export default {
-  content: ["./src/**/*.tsx", "./components/**/*.tsx"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-} satisfies Config;
---
+++ src/trpc/query-client.ts
@@ -0,0 +1,16 @@
+import {
+  defaultShouldDehydrateQuery,
+  QueryClient,
+} from "@tanstack/react-query";
+import SuperJSON from "superjson";
+
+export const createQueryClient = () =>
+  new QueryClient({
+    defaultOptions: {
+      queries: {
+        // With SSR, we usually want to set some default staleTime
+        // above 0 to avoid refetching immediately on the client
+        staleTime: 30 * 1000,
+      },
+    },
+  });
